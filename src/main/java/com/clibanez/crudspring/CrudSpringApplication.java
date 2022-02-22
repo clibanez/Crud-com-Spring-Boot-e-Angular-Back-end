4f20828e462acafe47dd2de7c8d722d88d8e6d7f
Clibanez <clibanezzcaldas@gmail.com>
2022-02-22 11:41:44 -0300

    Ateração Porn

--- src/main/java/com/clibanez/crudspring/CrudSpringApplication.java
+++ src/main/java/com/clibanez/crudspring/CrudSpringApplication.java
@@ -2,29 +2,12 @@ package com.clibanez.crudspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
-	}
-
-	@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/h2-console/**").permitAll();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-    }
-
-}
-}
+	} 
+  }
